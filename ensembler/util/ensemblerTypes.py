c917dfae742dc4b1b83ce04338e4cb7da3ed3ca1
B_Schroeder <SchroederB@users.noreply.github.com>
2020-07-22 10:25:39 +0200

    Update ensemblerTypes.py
    fixing test suit Part III

--- ensembler/util/ensemblerTypes.py
+++ ensembler/util/ensemblerTypes.py
@@ -21,5 +21,8 @@ system = TypeVar("system")  #dummyDef for integrator
 from ensembler.integrator._basicIntegrators import _integratorCls
 integrator = TypeVar("integrator", bound=_integratorCls)
 
+from ensembler.system import basic_system
+print(vars(basic_system))
+
 from ensembler.system.basic_system import system
 system = TypeVar("system", bound=system)
