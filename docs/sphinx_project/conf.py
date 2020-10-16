051583a065ef820ee628406a214ebc71239ca4f2
bschroed <bschroed@ethz.ch>
2020-10-16 18:01:41 +0200

    Update again documentation

--- docs/sphinx_project/conf.py
+++ docs/sphinx_project/conf.py
@@ -17,7 +17,8 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('../..'))
-print(os.path.abspath('../../..'))
+sys.path.insert(0, os.path.abspath('../../..'))
+
 
 # -- Project information -----------------------------------------------------
 
