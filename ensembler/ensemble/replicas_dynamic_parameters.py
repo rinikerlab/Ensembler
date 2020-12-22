49bb290e6d9b780e639dedadce3c29b51dc664df
dfhahn <davidfriedrichhahn@gmail.com>
2020-12-22 11:27:25 +0100

    Update replicas_dynamic_parameters.py.

--- ensembler/ensemble/replicas_dynamic_parameters.py
+++ ensembler/ensemble/replicas_dynamic_parameters.py
@@ -26,7 +26,7 @@ class conveyorBelt(_mutliReplicaApproach):
     coordinate_dimensions: int = 1
     replica_graph_dimensions: int = 1
     exchange_dimensions: Dict[str, np.array]
-    nSteps_between_trials: int = 2
+    n_steps_between_trials: int = 1
 
     exchange_information: pd.DataFrame = pd.DataFrame(columns=["Step", "capital_lambda", "TotE", "biasE", "doAccept"])
     system_trajs: dict = {}
@@ -46,7 +46,7 @@ class conveyorBelt(_mutliReplicaApproach):
 
 
     def __str__(self):
-        outstr = '{:5s}{:10s}{:10s}\n'.format("i",
+        outstr = '{:<5s}{:<10s}{:<10s}\n'.format("i",
                                                "lambda_i",
                                                "E_i"
                                                )
@@ -147,7 +147,7 @@ class conveyorBelt(_mutliReplicaApproach):
 
     def simulate(self,
                  ntrials: int,
-                 steps_between_trials: int = None,
+                 n_steps_between_trials: int = 1,
                  reset_ensemble: bool = False,
                  verbosity: bool = True):
         """
@@ -157,7 +157,7 @@ class conveyorBelt(_mutliReplicaApproach):
         ----------
         ntrials:int
             Number of conveyor belt steps
-        steps_between_trials: int, optional
+        n_steps_between_trials: int, optional
             number of integration steps of replicas between a move of the conveyor belt  (Default: None)
         reset_ensemble: bool, optional
             reset ensemble for starting the simulation? (Default: False)
@@ -170,8 +170,8 @@ class conveyorBelt(_mutliReplicaApproach):
 
         """
 
-        if (isinstance(steps_between_trials, int)):
-            self.set_simulation_steps_between_trials(n_steps=steps_between_trials)
+        if (isinstance(n_steps_between_trials, int)):
+            self.set_simulation_n_steps_between_trials(n_steps=n_steps_between_trials)
 
         self.__tmp_exchange_traj = []
         for _ in tqdm(range(ntrials), desc="Trials: ", mininterval=1.0, leave=verbosity):
@@ -184,12 +184,12 @@ class conveyorBelt(_mutliReplicaApproach):
 
     def run(self, verbosity: bool = False) -> NoReturn:
         """
-                Integrates the systems of the ensemble for the :var:nSteps_between_trials.
+                Integrates the systems of the ensemble for the :var:n_steps_between_trials.
         """
 
         self._currentTrial += 1
         for replica_coords, replica in self.replicas.items():
-            replica.simulate(steps=self.nSteps_between_trials, verbosity=verbosity)
+            replica.simulate(steps=self.n_steps_between_trials, verbosity=verbosity)
 
     def accept_move(self) -> NoReturn:
         """
@@ -209,12 +209,13 @@ class conveyorBelt(_mutliReplicaApproach):
         self.update_all_lambda(self.capital_lambda)
 
         newEne = self.calculate_total_ensemble_energy()
+        print(newEne)
         if self._default_metropolis_criterion(originalParams=oldEne, swappedParams=newEne):
             for i in self.replicas:
                 self.replicas[i]._update_dHdLambda()
 
             self.__tmp_exchange_traj.append({"Step": self._currentTrial, "capital_lambda": self.capital_lambda, "TotE": float(newEne),
-                 "biasE": float(newEne), "doAccept": True})
+                 "biasE": self.biasene, "doAccept": True})
         else:
             self.reject += 1
             self.update_all_lambda(oldBlam)
@@ -399,7 +400,7 @@ class conveyorBelt(_mutliReplicaApproach):
         self.system_trajs = {}
         self.exchange_information = pd.DataFrame(columns=["Step", "capital_lambda", "TotE", "biasE", "doAccept"])
 
-    def set_simulation_steps_between_trials(self, n_steps: int) -> NoReturn:
+    def set_simulation_n_steps_between_trials(self, n_steps: int) -> NoReturn:
         """
                     Sets the integration steps of the replicas between a trail move.
 
@@ -408,9 +409,9 @@ class conveyorBelt(_mutliReplicaApproach):
         n_steps:int
             number of steps
         """
-        self.nSteps_between_trials = n_steps
+        self.n_steps_between_trials = n_steps
         for coord, replica in self.replicas.items():
-            replica.nsteps = self.nSteps_between_trials
+            replica.nsteps = self.n_steps_between_trials
 
 
     @staticmethod
