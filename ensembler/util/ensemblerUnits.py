babf05b18fbecd602d2e9a4c0127f512b8cf6b2e
schroederb <benjamin-ries@outlook.com>
2022-02-09 10:28:30 +0100

    unitfiles

---
+++ ensembler/util/ensemblerUnits.py
@@ -0,0 +1,34 @@
+# Disable Pint's old fallback behavior (must come before importing Pint)
+import os
+os.environ['PINT_ARRAY_PROTOCOL_FALLBACK'] = "0"
+
+import pint
+from pint.quantity import _Quantity
+
+from sympy import sympify
+_Quantity._sympy_ = lambda s: sympify(f'{s.m}*{s.u:~}')
+
+#Build a unitRegistry
+unit_registry = pint.UnitRegistry()
+quantity = unit_registry.Quantity
+
+
+#Recommended Units
+## Energies
+kJ = unit_registry.kJ
+kcal = unit_registry.kcal
+kb = unit_registry.k
+
+## Temperature
+K = unit_registry.K
+
+## Geometrie: Distance
+nm = unit_registry.nm
+a = unit_registry.angstrom
+deg = unit_registry.degree
+
+
+C = unit_registry.C
+
+#Suggested Constants:
+k_harm = 1 * kJ / nm #used in gromos
