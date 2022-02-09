9cc2d1cdb0f5a76bfc2bf39ebc002f278e592fe5
schroederb <benjamin-ries@outlook.com>
2022-02-09 11:16:01 +0100

    constants file

--- ensembler/util/constants.py
+++ ensembler/util/constants.py
@@ -1,1 +1,12 @@
 from scipy import constants as const
+from scipy.constants import physical_constants as pc
+
+from units import nm, kJ
+
+atom_masses = {"C": 1}
+
+
+#C-C
+k_harm_CC = 7.1500000e+06 * (kJ/nm**2)
+k_quart_CC = 3.3474870e+05 * (kJ/nm**4)
+r_0_CC = 1.5300000e-01*nm
