cfeff438b65afeb3afc5fcc6a449e2d6e62b62ac
schroederb <benjamin-ries@outlook.com>
2022-02-25 14:37:46 +0100

    addChem

---
+++ ensembler/chemistry/forceFieldTerms.py
@@ -0,0 +1,13 @@
+from ensembler.potentials.OneD import harmonicOscillatorPotential
+
+from ensembler.util import constants
+from ensembler.util import units
+
+
+class bondTerm(harmonicOscillatorPotential):
+    """
+    _summary_
+    """
+
+    def __init__(self, k: float = constants.k_harm_CC, r_0: float = constants.r_0_CC, V_0: float = 0.0 * units.kJ):
+        super().__init__(k=k, x_shift=r_0, y_shift=V_0, unitless=False)
